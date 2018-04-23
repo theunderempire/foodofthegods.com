d5280d4db01e8424f60e0c0de51ee9f7d1c88263
thestablesonfire <accordiondeath@gmail.com>
2018-04-23 16:59:47 -0400

    auth and everything, boy i missed out on committing

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Uh oh!!' });
+  res.render('index', { title: 'Recipe action, if you know how' });
 });
 
 module.exports = router;
