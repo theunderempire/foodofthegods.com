8b723ed8721b0c4fae83b6bab502e2da5d037cda
thestablesonfire <accordiondeath@gmail.com>
2018-05-24 14:34:57 -0400

    print msg for script

--- api/mongoAddUser.js
+++ api/mongoAddUser.js
@@ -2,3 +2,5 @@ db = db.getSiblingDB('foodofthegods-api');
 print('selecting db: foodofthegods-api');
 
 db.users.insertOne({"username" : username, "password" : password, "timestamp" : timestamp});
+print('user inserted!');
+
