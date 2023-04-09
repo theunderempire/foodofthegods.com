5cdef932d6b6f1eeb0f3dec4156d446724f85c98
thestablesonfire <accordiondeath@gmail.com>
2023-04-09 16:22:57 -0400

    change username & pwd response shapes

--- api/routes/token.js
+++ api/routes/token.js
@@ -1,46 +1,64 @@
-var express = require('express');
+var express = require("express");
 
 var router = express.Router();
-var ObjectId = require('mongodb').ObjectID;
-var jwt = require('jsonwebtoken');
-var app = require('../fotg');
+var ObjectId = require("mongodb").ObjectID;
+var jwt = require("jsonwebtoken");
+var app = require("../fotg");
 
-router.get('/:username', function (req, res, next) {
+router.get("/:username", function (req, res, next) {
   var db = getDB(req);
   var collection = getCollection(db);
-  var app = require('../fotg');
+  var app = require("../fotg");
   var username = req.params.username;
 
   collection.findOne({ username: username }, {}, function (e, user) {
     if (!user) {
-      res.json({ success: false, message: 'Authentication failed. Incorrect credentials.' });
+      res.json({
+        success: false,
+        data: { message: "Authentication failed. Incorrect credentials." },
+      });
     } else {
-      res.json({ success: true, username: username, timestamp: user.timestamp });
+      res.json({
+        success: true,
+        data: { username: username, timestamp: user.timestamp },
+      });
     }
   });
-})
+});
 
-router.post('/', function (req, res, next) {
+router.post("/", function (req, res, next) {
   var db = getDB(req);
   var collection = getCollection(db);
-  var app = require('../fotg');
+  var app = require("../fotg");
 
   collection.findOne({ username: req.body.username }, {}, function (e, user) {
-
     if (!user) {
-      res.json({ success: false, message: 'Authentication failed. Incorrect credentials.' });
+      res.json({
+        success: false,
+        data: { message: "Authentication failed. Incorrect credentials." },
+      });
     } else {
       if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Incorrect credentials.' });
-      } else {
-        var token = jwt.sign({ success: true, username: user.username, password: user.password }, app.get('superSecret'), {
-          expiresIn: '1d'
+        res.json({
+          success: false,
+          data: { message: "Authentication failed. Incorrect credentials." },
         });
+      } else {
+        var token = jwt.sign(
+          {
+            success: true,
+            data: { username: user.username, password: user.password },
+          },
+          app.get("superSecret"),
+          {
+            expiresIn: "1d",
+          }
+        );
 
         res.json({
           success: true,
-          message: 'authenticated',
-          token: token
+          message: "authenticated",
+          token: token,
         });
       }
     }
@@ -48,7 +66,7 @@ router.post('/', function (req, res, next) {
 });
 
 function getCollection(db) {
-  return db.get('users');
+  return db.get("users");
 }
 
 function getDB(req) {
@@ -56,4 +74,3 @@ function getDB(req) {
 }
 
 module.exports = router;
-
