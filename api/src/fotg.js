15245bf9d9b8f9e575d62dcefe86fee3ba2509cc
Taylor Hawkins <thawkins@schmidtfutures.com>
2026-03-11 14:43:46 -0400

    just one env file

--- api/src/fotg.js
+++ api/src/fotg.js
@@ -8,7 +8,7 @@ import monk from "monk";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-dotenv.config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 const db = monk(
   `${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST_NAME}:27017/${process.env.DB_NAME}?authSource=admin`,
